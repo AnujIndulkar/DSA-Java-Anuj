be00622845dcd2a15023871d128885adca6607b1
Anuj Hindurao Indulkar <indulkaranuj89@gmail.com>
2026-03-07 22:06:07 +0530

    Complete Day 11: Three Sum Challenge

---
+++ Day11_ThreeSum.java
@@ -0,0 +1,72 @@
+package Arrays;
+
+import java.util.*;
+
+public class Day11_ThreeSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for(int i = 0; i < n; i++){
+            nums[i] = sc.nextInt();
+        }
+        System.out.println(bruteForce(nums));
+        System.out.println(optimal(nums));
+    }
+    // Brute Force Approach
+    // Time Complexity: O(n^3)
+    // Space Complexity: O(1)
+    public static List<List<Integer>> bruteForce(int[] nums){
+        Set<List<Integer>> set = new HashSet<>();
+        for(int i = 0; i < nums.length - 2; i++){
+            for(int j = i + 1; j < nums.length - 1; j++){
+                for(int k = j + 1; k < nums.length; k++){
+                    if(nums[i] + nums[j] + nums[k] == 0){
+                        List<Integer> temp = new ArrayList<>();
+                        temp.add(nums[i]);
+                        temp.add(nums[j]);
+                        temp.add(nums[k]);
+                        Collections.sort(temp);
+                        set.add(temp);
+                    }
+                }
+            }
+        }
+        return new ArrayList<>(set);
+    }
+    // Optimal Approach (Sorting + Two Pointer)
+    // Time Complexity: O(n^2)
+    // Space Complexity: O(1)
+    public static List<List<Integer>> optimal(int[] nums){
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length - 2; i++){
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
+            int left = i + 1;
+            int right = nums.length - 1;
+            while(left < right){
+                int sum = nums[i] + nums[left] + nums[right];
+                if(sum == 0){
+                    List<Integer> temp = new ArrayList<>();
+                    temp.add(nums[i]);
+                    temp.add(nums[left]);
+                    temp.add(nums[right]);
+                    result.add(temp);
+                    while(left < right && nums[left] == nums[left + 1]) left++;
+                    while(left < right && nums[right] == nums[right - 1]) right--;
+                    left++;
+                    right--;
+                }
+                else if(sum < 0){
+                    left++;
+                }
+                else{
+                    right--;
+                }
+            }
+        }
+        return result;
+    }
+}
+
+
